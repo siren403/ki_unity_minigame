82203071246f80d86f21189b2b917252e903fe2b
6kigs11-PC\6kigs11 <rlatjrfo2@naver.com>
2017-03-17 16:16:18 +0900

    프로토타이핑완료.

---
+++ MiniGame/Assets/KimSeokRae/Scripts/CScenePlayGame.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CScenePlayGame : MonoBehaviour {
+
+    public CPencil PFPenCil = null;
+    //public CPencil mpPenCil = null;
+
+
+    public List<CPencil> mPencils = null;
+    public int mPencilnum = 0;
+
+    public CUIPlayGame mpUi = null;
+    public int mPencilMoney = 0;
+    public Text mTxtMoney = null;
+	// Use this for initialization
+	void Start () {
+
+
+        /*
+        Debug.Log("GameStart!!");
+        //볼펜몸체 생성
+         mpPenCil = Instantiate<CPencil>(PFPenCil,new Vector3(0, 0, 0), Quaternion.identity);
+         //mpPenCil.Zhead();
+         mpPenCil.SetScene(this);
+         */
+        Appear();
+	}
+	
+    public void Appear()
+    {
+        mPencils[mPencilnum].transform.position = new Vector3(0, 0, 0);
+        mPencils[mPencilnum].gameObject.SetActive(true);
+    }
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void OnPencilPush()
+    {
+        if(mPencils[mPencilnum].OnPush(!mPencils[mPencilnum].mIsClose))
+        {
+            mPencilnum++;
+            if(mPencils.Count<=mPencilnum)
+            {
+                mPencilnum = 0;
+            }
+            Appear();
+            mPencilMoney++;
+            mTxtMoney.text = string.Format("Money : {0}", mPencilMoney);
+        }
+
+    }
+
+
+    public void OnPencilClose()
+    {
+        mPencils[mPencilnum].OnTurn(!mPencils[mPencilnum].mIsTurn);
+    }
+
+}
---
+++ MiniGame/Assets/KimSeokRae/Scripts/CUIPlayGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUIPlayGame : MonoBehaviour {
+
+   // public CPenCil mpPenCilHead = null;
+
+    public CScenePlayGame mpScene = null;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+    public void SetScene(CScenePlayGame tpScene)
+    {
+        mpScene = tpScene;
+    }
+
+    public void OnClickPressTurn()
+    {
+        Debug.Log("Spin!");
+
+        mpScene.OnPencilClose();
+        //mpScene.mpPenCil.AddScore();
+       // mpScene.mpPenCil.AddScore(10);
+       
+    }
+
+    public void OnClickPressClose()
+    {
+        Debug.Log("Close!");
+
+        mpScene.OnPencilPush();
+       // mpScene.mpPenCil.AddScore();
+        // mpScene.mpPenCil.AddScore(10);
+    }
+
+}
